69419e1e8c772cebc0372706313637beb4bb7aed
rorysroes <rorysroes@yahoo.com.tw>
2015-04-17 16:36:09 +0800

    create rand sequence

--- sorting.cpp
+++ sorting.cpp
@@ -3,5 +3,15 @@
 
 int main()
 {
-	int i;
+	int i,n,*v;
+	
+	//創造n個隨機整數 
+	n = 10;
+	v = (int*) malloc(n * sizeof(int));
+	for(i=0;i<n;i++)
+	{
+		v[i] = rand() % 100;
+		printf("%d,",v[i]);
+	}
+	printf("\n");
 }
