cb9bc204bbe8ca472d345de8a67e7daae863ad5e
rorysroes <rorysroes@yahoo.com.tw>
2015-04-17 17:11:03 +0800

    min to max

--- sorting.cpp
+++ sorting.cpp
@@ -3,7 +3,7 @@
 #include <time.h>
 int main()
 {
-	int i,n,*v;
+	int i,j,n,*v;
 	time_t T=1;
 	int hold;
 	//創造n個隨機整數
@@ -23,7 +23,27 @@ int main()
     //由左到右,相鄰兩兩比較,較大者往右移
 	//最大值會出現在陣列右端
 	//遞迴處理尚未排序的n-1個原元素
-	for(i=0;i<n;i++)
+	
+	for(j=1;j<n;++j)
+	{
+	
+		for(i=0;i<n-j;++i)
+		{
+			if(v[i]>v[i+1])    //相鄰兩兩比較 
+			{
+				hold = v[i];
+				v[i] = v[i+1];
+				v[i+1] = hold;
+			}
+	    } 
+	    for(i=0;i<n;++i)
+		{
+			printf("%d,",v[i]);
+		}
+		printf("\n");
+    }
+	/* 不能一直複製貼上 
+    for(i=0;i<n;i++)
 	{
 		if(v[i]>v[i+1])    //相鄰兩兩比較 
 		{
@@ -36,7 +56,8 @@ int main()
 	{
 		printf("%d,",v[i]);
 	}
-    
+	*/
+	
 }
 
 
