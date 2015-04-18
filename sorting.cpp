f88068b515321e6dd2c466a992f40510d76bc3cb
rorysroes <rorysroes@yahoo.com.tw>
2015-04-18 22:15:47 +0800

    1015

--- sorting.cpp
+++ sorting.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int main()
 {
-	int i,j,n,*v;
+	int i,n,*v;
 	time_t T=1;
 	int hold;
-	//創造n個隨機整數
-	
-	srand(time(&T));  //把現在時間當做隨機引數,不然會是固定的一組數列 
+	srand(time(&T));
 	n = 10;
 	v = (int*) malloc(n * sizeof(int));
 	for(i=0;i<n;i++)
@@ -17,6 +16,9 @@ int main()
 		printf("%d,",v[i]);
 	}
 	printf("\n");
+	
+	
+
 
 	
 }
