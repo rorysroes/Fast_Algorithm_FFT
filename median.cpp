3e8afb39f9294c762591ff44cb60f01f7d248216
rorysroes <rorysroes@yahoo.com.tw>
2015-04-25 21:32:47 +0800

    425

---
+++ median.cpp
@@ -0,0 +1,98 @@
+#include "stdio.h"
+#include "time.h"
+#include <stdlib.h>
+void quick_sort(int s[10],int left,int right);
+
+int main(){
+	int s[10]={-1},i,num = 9,a;
+	//創造一組亂數
+	srand(time(NULL));
+	for (int i = 0; i < num; i++){
+	s[i]=rand()%100;
+	}
+	for (i = 0; i < num; i++){
+	printf("%d\t",s[i]);
+	}
+	printf("\n");
+	quick_sort(s,0,num-1);
+
+	for ( i = 0; i < num; i++){
+	printf("%d\t",s[i]);
+	}
+	printf("\n");
+
+}
+//快速找中位數法(median) 
+//第一次pivot排完之後,比對pivot與中位數位置的差距,從這差距去尋找中位數 
+
+void quick_sort(int s[10],int left,int right)
+{ 
+	  if(right>left){
+		    int i,b,a = left-1;
+			for (i=left;i<=right;i++){
+					
+					if(s[i]<=s[right]){
+					    a++;
+						b=s[i];
+						s[i]=s[a];
+						s[a]=b;
+				    }
+				printf("\na=%d\ni=%d\n",a,i);
+				for (int j = 0; j < 9; j++){
+				printf("%d\t",s[j]);
+				}
+				printf("\n");
+            }
+      		if(a>4){
+	            quick_sort(s, left , a-1);
+	            
+		    }else if(a<4){
+		        quick_sort(s, a+1, right);	
+		    }else if(a=4){
+		    	printf("median = %d\n",s[a]);
+			}
+			if(5-a>0){
+				printf("median = %d",s[5-a]);
+			}else if(5-a<0){
+				printf("median = %d",s[])
+			} 			        
+	   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
